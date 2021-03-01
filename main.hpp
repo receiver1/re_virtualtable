6e694c4048f3554abbb633b4fbc28e0bd11d4f21
Receiver1 <62743649+Receiver1@users.noreply.github.com>
2021-03-01 17:29:22 +0300

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -1,9 +1,25 @@
 #pragma once
 
+enum class eResult
+{
+	failedProtection,
+	failedGetVT,
+	successHooked,
+	missingOrigOrVT
+};
+
+enum class eConvention
+{
+	cdeclcall,
+	stdcall,
+	thiscall,
+	fastcall
+};
+
 template<typename Type, typename... Args>
 class clVirtualTable
 {
-	UINT hookAddress, hookIndex;
+	UINT hookIndex;
 	LPVOID* virtualTable, originalFunction;
 public:
 	~clVirtualTable()
@@ -11,43 +27,67 @@ public:
 		this->remove();
 	}
 
-	void install(const UINT address, const UINT index, const LPVOID function)
+	eResult install(const UINT address, const UINT index, const LPVOID function)
 	{
-		hookAddress = address;
+		const LPVOID instance = *reinterpret_cast<LPVOID*>(address);
+		return this->install(instance, index, function);
+	}
+
+	eResult install(const LPVOID instance, const UINT index, const LPVOID function)
+	{
 		hookIndex = index;
 
-		virtualTable = *reinterpret_cast<LPVOID**>
-			(*reinterpret_cast<LPDWORD>(address));
+		virtualTable = *reinterpret_cast<LPVOID**>(instance);
+		if (!virtualTable) return eResult::failedGetVT;
 
 		DWORD dwProtection;
-		::VirtualProtect(reinterpret_cast<LPVOID>
-			(virtualTable + index), 4, PAGE_READWRITE, &dwProtection);
+		if (!VirtualProtect(reinterpret_cast<LPVOID>
+			(virtualTable + index), 4, PAGE_READWRITE, &dwProtection))
+			return eResult::failedProtection;
 
 		originalFunction = virtualTable[index];
 		virtualTable[index] = function;
 
-		::VirtualProtect(reinterpret_cast<LPVOID>
-			(virtualTable + index), 4, dwProtection, &dwProtection);
+		if (VirtualProtect(reinterpret_cast<LPVOID>
+			(virtualTable + index), 4, dwProtection, &dwProtection))
+			return eResult::failedProtection;
+
+		return eResult::successHooked;
 	}
 
-	Type call(Args... functionArguments)
+	Type call(eConvention callingConvention, Args... functionArguments)
 	{
-		return reinterpret_cast<Type(__stdcall*)(Args...)>(originalFunction)(functionArguments...);
+		switch (callingConvention)
+		{
+			case eConvention::cdeclcall: return reinterpret_cast
+				<Type(__cdecl*)(Args...)>(originalFunction)(functionArguments...);
+			case eConvention::stdcall: return reinterpret_cast
+				<Type(__stdcall*)(Args...)>(originalFunction)(functionArguments...);
+			case eConvention::thiscall: return reinterpret_cast
+				<Type(__thiscall*)(Args...)>(originalFunction)(functionArguments...);
+			case eConvention::fastcall: return reinterpret_cast
+				<Type(__fastcall*)(Args...)>(originalFunction)(functionArguments...);
+		}
 	}
 
-	void remove()
+	eResult remove()
 	{
-		if (virtualTable != nullptr && 
+		if (virtualTable != nullptr &&
 			originalFunction != nullptr)
 		{
 			DWORD dwProtection;
-			::VirtualProtect(reinterpret_cast<LPVOID>
-				(virtualTable + hookIndex), 4, PAGE_READWRITE, &dwProtection);
+			if (VirtualProtect(reinterpret_cast<LPVOID>
+				(virtualTable + hookIndex), 4, PAGE_READWRITE, &dwProtection))
+				return eResult::failedProtection;
 
 			virtualTable[hookIndex] = originalFunction;
 
-			::VirtualProtect(reinterpret_cast<LPVOID>
-				(virtualTable + hookIndex), 4, dwProtection, &dwProtection);
+			if (VirtualProtect(reinterpret_cast<LPVOID>
+				(virtualTable + hookIndex), 4, dwProtection, &dwProtection))
+				return eResult::failedProtection;
+
+			return eResult::successHooked;
 		}
+		return eResult::missingOrigOrVT;
 	}
 };
